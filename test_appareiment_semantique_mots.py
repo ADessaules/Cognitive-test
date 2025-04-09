68b61cea123c5e32dd3d0c3db36e4292fdf421c8
ADessaules <113447596+ADessaules@users.noreply.github.com>
2025-04-09 18:31:28 +0200

    test appariement sémantique avec les mots fini

--- test_appareiment_semantique_mots.py
+++ test_appareiment_semantique_mots.py
@@ -3,9 +3,81 @@ from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLay
 from PyQt6.QtCore import Qt
 
 tests = [
-    ("Chien", "Loup", "Chat"),
-    ("Voiture", "Vélo", "Eléphant"),
-    ("Pomme", "Banane", "Voiture")
+    ("souris", "chien", "chat"),
+    ("balais", "aspirateur", "cintre"),
+    ("âne", "carotte", "banane"),
+    ("chameau", "désert", "forêt"),
+    ("piano", "partition", "roman"),
+    ("sifflet", "maçon", "gendarme"),
+    ("château", "chevalier", "pirate"),
+    ("chauve-souris", "nuit", "jour"),
+    ("serpent", "banquise", "désert"),
+    ("flèche", "arc", "fusil"), 
+    ("moulin à vent", "campagne", "ville"),
+    ("fraise", "poêle", "sucre"),
+    ("arrosoir", "arbre", "fleur"),
+    ("tortue", "salade", "gland"),
+    ("scie", "tarte", "branche"),
+    ("maïs", "forêt", "champs"),
+    ("perroquet", "perchoir", "cheminée"),
+    ("tulipe", "jardinier", "cuisinier"),
+    ("brouette", "ciseaux", "pelle"),
+    ("vin", "tasse", "bouteille"),
+    ("compas", "sac à main", "cartable"),
+    ("luge", "rames", "skis"),
+    ("tambour", "baquettes", "louche"),
+    ("pyjama", "chaussures", "pantoufles"),
+    ("lapin", "carotte", "os"),
+    ("lézard", "lune", "soleil"),
+    ("cravate", "chemise", "robe"),
+    ("flûte", "pipe", "trompette"),
+    ("chapeau", "tête", "main"),
+    ("ananas", "tronçonneuse", "couteaux"),
+    ("mouton", "loup", "lion"),
+    ("aigle", "mer", "montagne"),
+    ("toupie", "main", "pied"),
+    ("ours", "vodka", "miel"),
+    ("singe", "salade", "banane"),
+    ("hache", "bûche", "pain"),
+    ("zèbre", "ferme", "savane"),
+    ("cygne", "forêt", "étang"),
+    ("lunettes", "oeil", "bouche"),
+    ("mains", "gants", "chaussures"),
+    ("selle", "mouton", "cheval"),
+    ("ancre", "pirogue", "paquebot"),
+    ("oreillers", "lit", "chaise"),
+    ("allumettes", "ampoule", "bougie"),
+    ("pyramide", "palmier", "sapin"),
+    ("tente", "feu de camp", "radiateur"),
+    ("gruyère", "lapin", "souris"),
+    ("niche", "chien", "chat"),
+    ("rideau", "porte", "fenêtre"),
+    ("ventilateur", "lune", "soleil"),
+    ("antivol", "vélo", "voiture"),
+    ("masque", "clown", "homme d'affaire"),
+    ("tableau", "table de ping pong", "bureau"),
+    ("vin", "raisin", "cerise"),
+    ("cactus", "désert", "littoral"),
+    ("clou", "marteau", "hache"),
+    ("orange", "jus", "vin"),
+    ("avion", "oiseau", "requin"),
+    ("camion", "panier", "carton"),
+    ("canard", "étang", "mer"),
+    ("grenouille", "tournesol", "nénuphar"),
+    ("crocodile", "panier", "sac à main"),
+    ("brosse à dent", "nez", "bouche"),
+    ("éléphant", "cirque", "église"),
+    ("groupe de gens", "bus", "cabane"),
+    ("ciseaux", "feuille de papier", "planche de bois"),
+    ("clé", "porte", "fenêtre"),
+    ("pomme", "scarabée", "ver"),
+    ("poubelle", "brosse", "balai"),
+    ("pile", "bougie", "lampe torche"),
+    ("chien", "nichoir", "niche"),
+    ("marteau", "vis", "clou"),
+    ("rhinocéros", "lion", "chat"),
+    ("moto", "costume", "blouson"),
+    ("écureuil", "maïs", "gland")
 ]
 
 class SemanticMatching(QWidget):
@@ -55,8 +127,11 @@ class SemanticMatching(QWidget):
             "selected": selected_word
         })
 
+        self.current_index += 1
+        self.load_next_test()
+
     def end_test(self):
-        QMessageBox.information(self, "Fin du test")
+        QMessageBox.information(self, "Fin du test", "Merci d'avoir effectué le test")
         print("Réponses :")
         for r in self.responses:
             print(r)
