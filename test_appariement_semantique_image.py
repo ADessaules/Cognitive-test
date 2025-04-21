98de4089c6347cf636b9eac0eb5d0affce357c8b
ADessaules <113447596+ADessaules@users.noreply.github.com>
2025-04-21 11:32:19 +0200

    test d'appariement fonctionnel avec image chargée

--- test_appariement_semantique_image.py
+++ test_appariement_semantique_image.py
@@ -1,103 +1,173 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget, QHBoxLayout
+import sys
+import os
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QDialog, QListWidget, QScrollArea, QDialogButtonBox, QListWidgetItem
 from PyQt6.QtGui import QPixmap, QIcon
 from PyQt6.QtCore import Qt
-import sys, os
 
-class ImageSemanticMatching(QMainWindow):
-    def __init__(self, tests=None):
+class ImageSemanticMatching(QWidget):
+    def __init__(self, tests):
         super().__init__()
-
-        if tests is None:
-            tests = [
-                ("C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image10.png", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/IMG_5849.jpg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image12.png"),
-                ("C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image4.jpeg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image5.jpeg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image6.jpeg"),
-                ("C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image7.jpeg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image8.jpeg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image9.jpeg"),
-                ("C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image10.jpeg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image11.jpeg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test1/image_test_appariement/image12.jpeg")
-            ]
+        self.setWindowTitle("Test Appariement Sémantique - Images")
+        self.setGeometry(100, 100, 800, 600)
 
         self.tests = tests
         self.current_index = 0
         self.responses = []
 
-        self.setWindowTitle("Test d'appariement sémantique - Images")
-        self.setGeometry(100, 100, 800, 600)
+        # Widgets
+        self.label_test_image = QLabel()
+        self.label_test_image.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-
-        self.layout = QVBoxLayout()
-        self.image_test_label = QLabel()
-        self.image_test_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.choice_layout = QHBoxLayout()
         self.button_choice_1 = QPushButton()
         self.button_choice_2 = QPushButton()
-
-        self.button_choice_1.setFixedSize(300, 300)
-        self.button_choice_2.setFixedSize(300, 300)
-
-        self.choice_layout.addWidget(self.button_choice_1)
-        self.choice_layout.addWidget(self.button_choice_2)
-
-        self.layout.addWidget(self.image_test_label)
-        self.layout.addLayout(self.choice_layout)
-
-        # 🛠️ LIGNE MANQUANTE :
-        self.central_widget.setLayout(self.layout)
+        self.button_choice_1.setFixedSize(200, 200)
+        self.button_choice_2.setFixedSize(200, 200)
 
         self.button_choice_1.clicked.connect(lambda: self.record_response(0))
         self.button_choice_2.clicked.connect(lambda: self.record_response(1))
 
+        # Layouts
+        layout = QVBoxLayout()
+        layout.addWidget(self.label_test_image)
+
+        choices_layout = QHBoxLayout()
+        choices_layout.addWidget(self.button_choice_1)
+        choices_layout.addWidget(self.button_choice_2)
+
+        layout.addLayout(choices_layout)
+        self.setLayout(layout)
+
         self.load_next_test()
 
     def load_next_test(self):
-        if self.current_index >= len(self.tests):
+        if self.current_index < len(self.tests):
+            test_path, choice1_path, choice2_path = self.tests[self.current_index]
+
+            print(f"[INFO] Loading test #{self.current_index + 1}:")
+            print("Test Image:", test_path)
+            print("Choices:", choice1_path, choice2_path)
+
+            # Test image
+            pixmap_test = QPixmap(test_path)
+            if pixmap_test.isNull():
+                print(f"[ERREUR] Impossible de charger l'image test : {test_path}")
+            self.label_test_image.setPixmap(pixmap_test.scaled(300, 300, Qt.AspectRatioMode.KeepAspectRatio))
+
+            # Choix
+            pixmap1 = QPixmap(choice1_path)
+            pixmap2 = QPixmap(choice2_path)
+
+            self.button_choice_1.setIcon(QIcon(pixmap1))
+            self.button_choice_1.setIconSize(self.button_choice_1.size())
+
+            self.button_choice_2.setIcon(QIcon(pixmap2))
+            self.button_choice_2.setIconSize(self.button_choice_2.size())
+        else:
             self.end_test()
-            return
-
-        test_image_path, choice1_path, choice2_path = self.tests[self.current_index]
-
-        print("Test image:", test_image_path)
-        print("Choix 1:", choice1_path)
-        print("Choix 2:", choice2_path)
-
-
-        if not os.path.exists(test_image_path):
-            print(f"[ERREUR] Image test manquante : {test_image_path}")
-        if not os.path.exists(choice1_path):
-            print(f"[ERREUR] Image 1 manquante : {choice1_path}")
-        if not os.path.exists(choice2_path):
-            print(f"[ERREUR] Image 2 manquante : {choice2_path}")
-
-        self.image_test_label.setPixmap(QPixmap(test_image_path).scaledToHeight(200))
-
-        pixmap1 = QPixmap(choice1_path).scaled(280, 280, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
-        pixmap2 = QPixmap(choice2_path).scaled(280, 280, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
-
-        print(pixmap1.isNull(), pixmap2.isNull())
-
-        self.button_choice_1.setIcon(QIcon(pixmap1))
-        self.button_choice_1.setIconSize(pixmap1.size())
-
-        self.button_choice_2.setIcon(QIcon(pixmap2))
-        self.button_choice_2.setIconSize(pixmap2.size())
 
     def record_response(self, choice_index):
+        test = self.tests[self.current_index]
+        selected_path = test[1 + choice_index]
         self.responses.append({
-            "test": self.tests[self.current_index],
-            "selected": self.tests[self.current_index][choice_index + 1]
+            "test": test[0],
+            "choice1": test[1],
+            "choice2": test[2],
+            "selected": selected_path
         })
+
         self.current_index += 1
         self.load_next_test()
-
+       
     def end_test(self):
-        print("Test terminé !")
-        for i, response in enumerate(self.responses):
-            print(f"Item {i + 1} : {response['test'][0]} => {response['selected']}")
+        recap = "Test terminé ! \n\n Les Réponses :\n\n"
+        for r in self.responses:
+            recap += f"- Test : {r['test']}\n -> Réponse : {r['selected']}"
+
+        recap_dialog = QDialog(self)
+        recap_dialog.setWindowTitle("Résultats du test")
+        layout = QVBoxLayout()
+
+        scroll = QScrollArea()
+        scroll.setWidgetResizable(True)
+
+        content = QWidget()
+        content_layout = QVBoxLayout()
+        label = QLabel(recap)
+        label.setWordWrap(True)
+        content_layout.addWidget(label)
+        content.setLayout(content_layout)
+
+        scroll.setWidget(content)
+        layout.addWidget(scroll)
+
+        # Boutons Oui / Non
+        buttons = QDialogButtonBox(QDialogButtonBox.StandardButton.Yes | QDialogButtonBox.StandardButton.No)
+        buttons.button(QDialogButtonBox.StandardButton.Yes).setText("Repasser certains items")
+        buttons.button(QDialogButtonBox.StandardButton.No).setText("Quitter")
+
+        buttons.accepted.connect(recap_dialog.accept)
+        buttons.rejected.connect(recap_dialog.reject)
+
+        layout.addWidget(buttons)
+        recap_dialog.setLayout(layout)
+        recap_dialog.resize(500, 400)
+        if recap_dialog.exec():
+            self.select_item_to_retry()
+        else:
+            self.close()
+
+    def select_item_to_retry(self):
+        dialog = QDialog(self)
+        dialog.setWindowTitle("Repasser certains items")
+        layout = QVBoxLayout()
+
+        list_widget = QListWidget()
+        list_widget.setMinimumHeight(200)
+        list_widget.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+
+        self.item_map = {}
+        for i,r in enumerate(self.responses):
+            item_image= f"{r['test']} -> {r['selected']}"
+            item = QListWidgetItem(item_image)
+            item.setCheckState(Qt.CheckState.Unchecked)
+            list_widget.addItem(item)
+            self.item_map[i] = r
+
+        layout.addWidget(QLabel("Sélectionner les items que vous voulez faire repasser"))
+        layout.addWidget(list_widget)
+
+        buttons = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
+        buttons.accepted.connect(dialog.accept)
+        buttons.rejected.connect(dialog.reject)
+        layout.addWidget(buttons)
+
+        dialog.setLayout(layout)
+
+        if dialog.exec():
+            items_to_retry = []
+            for i in range(list_widget.count()):
+                item = list_widget.item(i)
+                if item.checkState() == Qt.CheckState.Checked:
+                    response = self.item_map[i]
+                    test = (response['test'], response['choice1'], response['choice2'])
+                    items_to_retry.append(test)
+            if items_to_retry:
+                self.retry_item(items_to_retry)
+
+    def retry_item(self, items):
+        self.new_window = ImageSemanticMatching(items)
+        self.new_window.show()
         self.close()
 
 if __name__ == "__main__":
+    tests_image = [
+        ("C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image1.jpg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image2.jpg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image3.jpg"),
+        ("C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image4.jpg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image5.jpg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image6.jpg"),
+        ("C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image7.jpg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image8.jpg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image9.jpg"),
+        ("C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image10.jpg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image11.jpg", "C:/Users/lekil/OneDrive/Bureau/Projet_tutoré-Cognitive_test/Cognitive-test/image_test_appariement/Image12.jpg"),
+    ]
+
     app = QApplication(sys.argv)
-    window = ImageSemanticMatching()
+    window = ImageSemanticMatching(tests_image)
     window.show()
     sys.exit(app.exec())
