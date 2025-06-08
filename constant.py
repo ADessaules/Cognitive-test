853b022acdf6f1f3ec5dffc57ffac815f56230e4
Corentinbldl <corentinblondelle1@gmail.com>
2025-06-08 20:58:46 +0200

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,5 +1,5 @@
 import os
 
 DB_FILE = "patients.db"
-DOSSIER_IMAGES = os.path.join("famous_faceV1", "image_famous_faceV1")
+DOSSIER_IMAGES = os.path.join("famous_faceV1", "image_preselection")
 DOSSIER_PATIENTS = "Patients"
