bde141d6be626d1c0cc03f9d48d73f0394481e6c
Corentinbldl <corentinblondelle1@gmail.com>
2025-06-11 22:55:44 +0200

    Update main_window.py

--- famous_faceV1/main_window.py
+++ famous_faceV1/main_window.py
@@ -25,44 +25,22 @@ class MainApp(QWidget):
         
         self.btn_test_famous_face = QPushButton("Aller au test Famous Face")
         self.btn_test_famous_face.clicked.connect(self.go_to_famous_face_test)
-        
-        self.btn_creer_patient = QPushButton("Créer Patient")
-        self.btn_creer_patient.setFixedHeight(60)
-        self.btn_creer_patient.setStyleSheet("font-size: 18px;")
 
         self.btn_liste_patients = QPushButton("Voir Patients")
         self.btn_liste_patients.setFixedHeight(60)
         self.btn_liste_patients.setStyleSheet("font-size: 18px;")
 
-        self.btn_supprimer_patient = QPushButton("Supprimer Patient")
-        self.btn_supprimer_patient.setFixedHeight(60)
-        self.btn_supprimer_patient.setStyleSheet("font-size: 18px;")
-
-
         self.btn_selection_celeb = QPushButton("Sélectionner Célébrités pour un Patient")
         self.btn_selection_celeb.setFixedHeight(60)
         self.btn_selection_celeb.setStyleSheet("font-size: 18px;")
-        
-        self.btn_bisection = QPushButton("Bisection")
-        self.btn_bisection.setFixedHeight(60)
-        self.btn_bisection.setStyleSheet("font-size: 18px;")        
-
 
         self.btn_selection_celeb.clicked.connect(self.selectionner_patient_pour_celebrite) 
-        self.btn_creer_patient.clicked.connect(lambda: creer_patient(self))
         self.btn_liste_patients.clicked.connect(self.afficher_liste_patients)
-        self.btn_supprimer_patient.clicked.connect(self.afficher_liste_patients_supprimer)
-        self.btn_bisection.clicked.connect(self.selectionner_patient_pour_bisection)        
 
         self.layout.addWidget(self.btn_interface_principale)
         self.layout.addWidget(self.btn_test_famous_face)
-        self.layout.addWidget(self.btn_creer_patient)
         self.layout.addWidget(self.btn_selection_celeb)
         self.layout.addWidget(self.btn_liste_patients)
-        self.layout.addWidget(self.btn_supprimer_patient)
-        self.layout.addWidget(self.btn_bisection)
-
-        
         self.setLayout(self.layout)
 
     def go_to_main_interface(self):
@@ -83,16 +61,8 @@ class MainApp(QWidget):
         self.liste_patients_fenetre = ListePatients()
         self.liste_patients_fenetre.show()
     
-    def afficher_liste_patients_supprimer(self):
-        self.supprimer_patients_fenetre = ListePatients(supprimer=True)
-        self.supprimer_patients_fenetre.show()
-
     def selectionner_patient_pour_celebrite(self):
         self.selection_patient_fenetre = SelectionPatientDialog(self.lancer_selection_celebrite)
-        self.selection_patient_fenetre.show()
-
-    def selectionner_patient_pour_bisection(self):
-        self.selection_patient_fenetre = SelectionPatientDialog(self.lancer_bisection)
         self.selection_patient_fenetre.show()
 
     def lancer_selection_celebrite(self, patient_id, patient_name):
@@ -108,15 +78,3 @@ class MainApp(QWidget):
         else:
             self.selection_fenetre = SelectionCelebrites(patient_id, patient_name)
             self.selection_fenetre.show()
-
-    def lancer_bisection(self, patient_id):
-        screens = QGuiApplication.screens()
-
-        if len(screens) >= 2:
-            screen_experimenter = screens[0]
-            screen_patient = screens[1]
-        else:
-            screen_experimenter = screen_patient = screens[0]
-
-        self.bisection_fenetre = BisectionTest(patient_id, screen_patient, screen_experimenter)
-        self.bisection_fenetre.show()
